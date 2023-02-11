fb6575c69e41ea4e97351d3b4a7b6b6f620b58d3
Evgeny Yuryev <evgeny.yuryev@careem.com>
2023-02-11 16:31:32 +0400

    telescope

--- .config/nvim/lua/odinmillion/plugins/telescope.lua
+++ .config/nvim/lua/odinmillion/plugins/telescope.lua
@@ -10,10 +10,44 @@ if not actions_setup then
   return
 end
 
+-- set current index in status text
+local pickers = require("telescope.pickers")
+
+local get_status_text = function(self)
+  local selected = #(self:get_multi_selection())
+  local num_results = (self.stats.processed or 0) - (self.stats.filtered or 0)
+  local current_index = num_results == 0 and 0 or self:get_index((self.stats.jy_current_row or 0))
+  if selected ~= 0 then
+    return string.format("(Selected: %s) %s / %s", selected, current_index, num_results)
+  end
+  return string.format("%s / %s", current_index, num_results)
+end
+
+local original_update_prefix = pickers._Picker.update_prefix
+
+pickers._Picker.update_prefix = function(self, entry, row)
+  self.stats.jy_current_row = row
+  self:get_status_updater(self.prompt_win, self.prompt_bufnr)()
+  return original_update_prefix(self, entry, row)
+end
+
+local original_reset_track = pickers._Picker._reset_track
+
+pickers._Picker._reset_track = function(self)
+  self.stats.jy_current_row = 0
+  original_reset_track(self)
+end
+
 -- configure telescope
 telescope.setup({
   -- configure custom mappings
   defaults = {
+    scroll_strategy = "limit",
+    layout_strategy = "vertical",
+    layout_config = { height = 0.95 },
+    dynamic_preview_title = true,
+    wrap_results = false,
+    get_status_text = get_status_text,
     mappings = {
       i = {
         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
