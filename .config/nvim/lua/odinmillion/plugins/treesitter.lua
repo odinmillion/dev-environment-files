c6d34930f5c4ae6d27aa318e6d70f38eff0ba55f
Evgeny Yuryev <evgeny.yuryev@careem.com>
2023-02-12 18:28:09 +0400

    add incremental selection

--- .config/nvim/lua/odinmillion/plugins/treesitter.lua
+++ .config/nvim/lua/odinmillion/plugins/treesitter.lua
@@ -1,37 +1,46 @@
 -- import nvim-treesitter plugin safely
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+  return
 end
 
 -- configure treesitter
 treesitter.setup({
-	-- enable syntax highlighting
-	highlight = {
-		enable = true,
-	},
-	-- enable indentation
-	indent = { enable = true },
-	-- enable autotagging (w/ nvim-ts-autotag plugin)
-	autotag = { enable = true },
-	-- ensure these language parsers are installed
-	ensure_installed = {
-		"json",
-		"javascript",
-		"typescript",
-		"tsx",
-		"yaml",
-		"html",
-		"css",
-		"markdown",
-		"svelte",
-		"graphql",
-		"bash",
-		"lua",
-		"vim",
-		"dockerfile",
-		"gitignore",
-	},
-	-- auto install above language parsers
-	auto_install = true,
+  -- enable syntax highlighting
+  highlight = {
+    enable = true,
+  },
+  -- enable indentation
+  indent = { enable = true },
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
+  -- ensure these language parsers are installed
+  ensure_installed = {
+    "json",
+    "javascript",
+    "typescript",
+    "tsx",
+    "yaml",
+    "html",
+    "css",
+    "markdown",
+    "svelte",
+    "graphql",
+    "bash",
+    "lua",
+    "vim",
+    "dockerfile",
+    "gitignore",
+  },
+  -- auto install above language parsers
+  auto_install = true,
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "grn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      node_decremental = "grm",
+      -- scope_incremental = "grc", -- don't use that
+    },
+  },
 })
