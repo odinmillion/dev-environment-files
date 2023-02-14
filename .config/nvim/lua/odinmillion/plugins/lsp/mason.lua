3dc2a1b5ac0420b5d1d76c11a3b5eb0d76ab60b7
Evgeny Yuryev <evgeny.yuryev@careem.com>
2023-02-14 08:56:07 +0400

    fix lua

--- .config/nvim/lua/odinmillion/plugins/lsp/mason.lua
+++ .config/nvim/lua/odinmillion/plugins/lsp/mason.lua
@@ -22,13 +22,13 @@ mason.setup()
 mason_lspconfig.setup({
   -- list of servers for mason to install
   ensure_installed = {
-    "tsserver",
-    "html",
-    "cssls",
-    "tailwindcss",
-    "sumneko_lua",
-    "emmet_ls",
-    "gopls",
+    'tsserver',
+    'html',
+    'cssls',
+    'tailwindcss',
+    'lua_ls',
+    'emmet_ls',
+    'gopls',
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
